d478638d1c2b5b4212de41799511b3450e527ae0
aaanderson57 <30771510+aaanderson57@users.noreply.github.com>
2025-01-17 07:41:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,8 @@ import numpy as np
 #from utils.combiner import CombinedAttributesAdder
 
 st.title("California Housing Prices Prediction")
+
+# layout and input data
+col1, col2 = st.columns([1, 2], gap='large')
+with col1:
+    st.header("Enter the attributes of the housing.")
