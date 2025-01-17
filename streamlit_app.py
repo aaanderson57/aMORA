5a9557d6e3a57ef756c2d64d8603999e9d09bcad
aaanderson57 <30771510+aaanderson57@users.noreply.github.com>
2025-01-17 07:38:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+st.set_page_config(page_title="Housing Prices Prediction", page_icon=":house:")
+import pandas as pd
+import numpy as np
+import pickle
+import folium
+from geopy.geocoders import Nominatim
+import geopy.distance
+from streamlit_folium import st_folium
+from utils.combiner import CombinedAttributesAdder
 
+st.title("California Housing Prices Prediction")
