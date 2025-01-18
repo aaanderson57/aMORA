4490e267ca8f8b799f924da9cd850b3a25fbedd8
aaanderson57 <30771510+aaanderson57@users.noreply.github.com>
2025-01-18 18:16:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ if st.button('Prever Preço'):
     print(input_data.dtypes)
     # Faz a predição usando o modelo carregado
     try:
-        prediction = model.predict(input_data)[0]
-        st.success(f'Preço de venda estimado: R$ {prediction:.2f}')
+        print(input_data.dtypes)
+        #prediction = model.predict(input_data)[0]
+        #st.success(f'Preço de venda estimado: R$ {prediction:.2f}')
     except ValueError as e:
         st.error(f"Erro ao realizar a predição: {e}. Verifique os valores inseridos.")
